3c4124ef978edbceda0c935b6305873418e6d2e9
Dave Goel <davegoel29@gmail.com>
2023-02-23 16:46:43 -0700

    posts

--- UltimateTeapot/main/models.py
+++ UltimateTeapot/main/models.py
@@ -4,16 +4,26 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 class Author(AbstractBaseUser):
     author_id = models.UUIDField()
+    username = None
+    identifier = models.CharField(max_length=40, unique=True, default=0)
+    USERNAME_FIELD = "identifier"
 
 class Post(models.Model):
     post_id = models.UUIDField()
+    #user = models.ForeignKey(AbstractBaseUser)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
-    image = models.ImageField()
+    image = models.ImageField(null=True, blank=True)
     pub_date = models.DateTimeField('date posted')
-    link = models.CharField(max_length=255)
+    #link = models.CharField(max_length=255, default=None)
     visibility = models.TextField()
-    likes = models.IntegerField()
+    likes = models.IntegerField(default=0) 
+
+    def __str__(self):
+        return(f"{self.author} "
+              f"({self.pub_date:%Y-%m-%d %H:%M}): "
+              f"{self.text}"
+        )
 
 class Comment(models.Model):
     text = models.CharField(max_length=255)
--- UltimateTeapot/main/views.py
+++ UltimateTeapot/main/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
     return HttpResponse("Hello, world. You're at the main\ index.")
+
+def posts(request):
+    posts = Post.objects.all()
+
+    return HttpResponse( posts)
