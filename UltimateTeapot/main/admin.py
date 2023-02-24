708a8e2a99c3e7b30598831c234a777bbc383303
Tran <phuonganhnguyenn0011@gmail.com>
2023-02-24 15:09:12 -0700

    Added basic login page, sign up page, logout function and basic authentication, changes the path for home page from /main to /, added profile model

--- UltimateTeapot/main/admin.py
+++ UltimateTeapot/main/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Post, Comment, PostLike, CommentLike, Author
+from .models import Post, Comment, PostLike, CommentLike, Author, Profile
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(PostLike)
 admin.site.register(CommentLike)
 admin.site.register(Author)
+admin.site.register(Profile)
--- UltimateTeapot/main/models.py
+++ UltimateTeapot/main/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
 
 class Author(AbstractBaseUser):
     author_id = models.UUIDField()
