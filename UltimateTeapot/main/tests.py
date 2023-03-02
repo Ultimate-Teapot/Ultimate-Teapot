428f1abe136609ef4f71f7d5bc22ce5d812ddc40
Gmaliakal <77520861+Gmaliakal@users.noreply.github.com>
2023-03-01 21:53:01 -0700

    basic test

--- UltimateTeapot/main/tests.py
+++ UltimateTeapot/main/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+
+class CreateUserTestCase(TestCase):
+    def test_create_user(self):
+        
+
+        newUser = User.objects.create_user(username="camelCase", email="camel@desert.com", password="chicken", first_name = "crazy", last_name = "camel")
+        newUser.save()
+        
+        new_profile = Profile.objects.create(user=newUser)
+        new_profile.save()
+
+        new_user = User.objects.get(username="camelCase")
+        profile = Profile.objects.get(user=new_user)
+        profile.followerno=5
+
+        print(profile.followerno)
+
+        assert(new_user.get_full_name()=="crazy camel")
+
+
+        # run tests using  python manage.py tests
+        # def test_create_user(self):
+
+        # def test_create_user(self):
+      
+        
+
+        
+
+
+
